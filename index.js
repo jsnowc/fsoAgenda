e6c56fa3354527190ea16c502aa0d3c20c125c75
Jesus Cruz <crdatasys@gmail.com>
2025-05-13 17:50:18 -0500

    Ejercicios 3.15.-3.18.

--- index.js
+++ index.js
@@ -14,29 +14,6 @@ morgan.token('post-content', function (req, res) { return req.method === 'POST' 
 
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :post-content'))
 
-let phonebook = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(person => {
     response.json(person)
@@ -52,41 +29,31 @@ app.get('/info', (request, response) => {
   })   
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })/*
-  const id = Number(request.params.id)
-  console.log(id)
-  const person = phonebook.find(person => person.id === id)
-  console.log(person)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }  */
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+  })
+  .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  console.log(id)
-  phonebook = phonebook.filter(person => person.id !== id)
- 
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-const generateId = () => {
-  return  Math.floor(Math.random() * 10000)
-}
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-  //console.log(body)
 
   if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'name o number missing' 
-    })
+    next({name:'RequestError'})
   }
   /* validacion si existia el nro
   const exist = phonebook.find(person => person.name === body.name)
@@ -106,17 +73,45 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
-  /* cod anterior
-  phonebook = phonebook.concat(person)
+})
 
-  response.json(person)*/
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+// controlador de solicitudes con endpoint desconocido
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  if (error.name === 'RequestError') {
+    return response.status(400).send({ error: 'name o number missing' })
+  } 
+
+  next(error)
+}
+
+// este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
